89bfaa88886bf7dec46c79d0051ba3ac9b4d7860
Sowjanya Maisagoni <maisagoni@gmail.com>
2021-08-26 11:06:18 -0400

    adding input.js

---
+++ input.js
@@ -0,0 +1,19 @@
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  if(key === '\u0003') {
+    process.exit();
+  }
+};
+
+module.exports = setupInput;
--- play.js
+++ play.js
@@ -1,26 +1,6 @@
 const  connect  = require('./client');
-   const setupInput = function() {
-   const stdin = process.stdin;
-   stdin.on('data', (key) => {
-      handleUserInput(key);
-    });
-   stdin.setRawMode(true);
-   stdin.setEncoding('utf8');
-   stdin.resume();
-   stdin.on('data', (key) => {
-     handleUserInput(key);
-   });
- return stdin;
- };
- 
- const handleUserInput = function(key) {
-   // \u0003 maps to ctrl+c input
- 
-   if (key === '\u0003') {
-     process.exit();
-   }
- 
- };
+
+const setupInput  = require("./input");
  console.log('Connecting ... ');
  
  setupInput();
