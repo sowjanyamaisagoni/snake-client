6aba1db08a77f93f11c390b0ee8a3774fe9adcb5
Sowjanya Maisagoni <maisagoni@gmail.com>
2021-08-26 10:56:31 -0400

    snake/stdin

--- play.js
+++ play.js
@@ -1,3 +1,29 @@
 const  connect  = require('./client');
-console.log("Connecting ...");
-connect(); 
+   const setupInput = function() {
+   const stdin = process.stdin;
+   stdin.on('data', (key) => {
+      handleUserInput(key);
+    });
+   stdin.setRawMode(true);
+   stdin.setEncoding('utf8');
+   stdin.resume();
+   stdin.on('data', (key) => {
+     handleUserInput(key);
+   });
+ return stdin;
+ };
+ 
+ const handleUserInput = function(key) {
+   // \u0003 maps to ctrl+c input
+ 
+   if (key === '\u0003') {
+     process.exit();
+   }
+ 
+ };
+ console.log('Connecting ... ');
+ 
+ setupInput();
+ connect(); 
+ 
+ 
